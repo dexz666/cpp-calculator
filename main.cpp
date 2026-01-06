4ac35e820c0b0ae8c878a0b6eaf40c959ff32592
dexz <dexz@google.com>
2026-01-06 23:34:56 +0000

    addition function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+int add(int a, int b){
+	return a + b;
+}
 int main(){
 	std::cout<<"Calculator V1.0"<<std::endl;
+	std::cout<<"2 + 2 = " << add(2,2)<< std::endl;
 	return 0;
 }

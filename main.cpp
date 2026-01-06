945dbe60e3a67d189fb6810e3d7f4a1ec5dae230
dexz <dexz@google.com>
2026-01-06 23:40:14 +0000

    add subtraction feature

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 int add(int a, int b){
 	return a + b;
 }
+
+int subtract(int a, int b){
+	return a - b;
+}
+
 int main(){
 	std::cout<<"Calculator V1.0"<<std::endl;
-	std::cout<<"2 + 2 = " << add(2,2)<< std::endl;
+	std::cout<<"2 + 2 = " << subtract(5,3)<< std::endl;
 	return 0;
 }

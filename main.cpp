e3ca9b3679572b3169cfa0e754c9711c530a3299
dexz <dexz@google.com>
2026-01-06 23:46:12 +0000

    add multiplication support

--- main.cpp
+++ main.cpp
@@ -8,8 +8,12 @@ int subtract(int a, int b){
 	return a - b;
 }
 
+int multiply(int a, int b){
+	return a * b;
+}
+
 int main(){
 	std::cout<<"Calculator V1.0"<<std::endl;
-	std::cout<<"2 + 2 = " << subtract(5,3)<< std::endl;
+	std::cout<<"2 + 2 = " << multiply(10,5)<< std::endl;
 	return 0;
 }
